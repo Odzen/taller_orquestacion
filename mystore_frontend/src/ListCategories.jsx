bd8876e20bb480977aee6024af6aaff4b8676c6f
Odzen <jsebastian.va@gmail.com>
2023-04-11 17:43:04 -0500

    deleted

--- mystore_frontend/src/ListCategories.jsx
+++ mystore_frontend/src/ListCategories.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react'
+import { useEffect} from 'react'
 import { Table} from "react-bootstrap";
 
 export const ListCategories = ({categories, setCategories}) => {
-//   const [categories, setCaregories] = useState([]);
     
     useEffect(() => {
         fetch("http://localhost:4000/categories/")
